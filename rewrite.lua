b8ced0b00a3dea04fd21f26b6e6d193eec7e34ae
sudoaptinstallopsec123 <miseryavenue200@gmail.com>
2026-03-25 22:58:43 +0000

    Update rewrite.lua

--- rewrite.lua
+++ rewrite.lua
@@ -936,22 +936,22 @@ local Options = Library.Options
 local Toggles = Library.Toggles
 
 ThemeManager.BuiltInThemes = {
-		['Default'] = { 1, httpService:JSONDecode('{"FontColor":"89919c","MainColor":"20232a","AccentColor":"898aae","BackgroundColor":"161a20","OutlineColor":"25262d"}') },
-        ['New'] = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"6a4c6b","AccentColor":"6d31ff","BackgroundColor":"d3aa32","OutlineColor":"000000"}') },
-		['BBot'] 			= { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
-		['Fatality']		= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
-		['Jester'] 			= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
-		['Mint'] 			= { 5, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"3db488","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
-		['Tokyo Night'] 	= { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
-		['Ubuntu'] 			= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"3e3e3e","AccentColor":"e2581e","BackgroundColor":"323232","OutlineColor":"191919"}') },
-		['Quartz'] 			= { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232330","AccentColor":"426e87","BackgroundColor":"1d1b26","OutlineColor":"27232f"}') },
+		['Default'] =         { 1, httpService:JSONDecode('{"FontColor":"89919c","MainColor":"20232a","AccentColor":"898aae","BackgroundColor":"161a20","OutlineColor":"25262d"}') },
+        ['New'] =             { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"6a4c6b","AccentColor":"6d31ff","BackgroundColor":"d3aa32","OutlineColor":"000000"}') },
+		['BBot'] 			= { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
+		['Fatality']		= { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
+		['Jester'] 			= { 5, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
+		['Mint'] 			= { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"3db488","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
+		['Tokyo Night'] 	= { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
+		['Ubuntu'] 			= { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"3e3e3e","AccentColor":"e2581e","BackgroundColor":"323232","OutlineColor":"191919"}') },
+		['Quartz'] 			= { 9, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232330","AccentColor":"426e87","BackgroundColor":"1d1b26","OutlineColor":"27232f"}') },
 	}
 
 local Window = Library:CreateWindow({
-
-Title = '                       perc<font color="#898aae">.hook</font> // ##B4RR          <font color="#898aae">.gg/UkPDe8hF4p</font>',
+    Title = 'perc<font color="#898aae">.hook</font>   ',
     Center = true,
     AutoShow = true,
+    Icon = 97903891100298,
     Footer = "perc.hook //  discord.gg/UkPDe8hF4p",
     TabPadding = 2,
     MenuFadeTime = 0.1
@@ -2917,6 +2917,42 @@ MenuGroup:AddSlider('BlurValue', {
     Callback = function(Value)
         MenuBlur.Size = Value
     end
+})
+
+MenuGroup:AddDropdown("DPIDropdown", {
+	Values = { "50%", "75%", "100%", "125%", "150%", "175%", "200%" },
+	Default = "100%",
+
+	Text = "DPI Scale",
+
+	Callback = function(Value)
+		Value = Value:gsub("%%", "")
+		local DPI = tonumber(Value)
+
+		Library:SetDPIScale(DPI)
+	end,
+})
+
+MenuGroup:AddSlider('BlurValue', {
+    Text = 'Blur Strength',
+    Default = 20,
+    Min = 1,
+    Max = 56,
+    Rounding = 1,
+    Callback = function(Value)
+        MenuBlur.Size = Value
+    end
+})
+
+MenuGroup:AddSlider("UICornerSlider", {
+	Text = "Corner Radius",
+	Default = Library.CornerRadius,
+	Min = 0,
+	Max = 20,
+	Rounding = 0,
+	Callback = function(value)
+		Window:SetCornerRadius(value)
+	end
 })
 
 MenuGroup:AddLabel('I LOVE MY WIFE ZEE', true)
